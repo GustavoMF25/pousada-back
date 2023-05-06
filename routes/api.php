8ea499b8b6c3e735d9f92b62050a262d4744ef18
Gustavo Fernandes -  TI Desenvolvimento de Sistemas <gustavo.fernandes@CORP>
2023-05-05 21:48:39 -0300

    feature: adding laradock in project

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/', function () {
-    return json_encode(['a' => 'a']);
-});
-
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
